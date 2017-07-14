d614bdd2097d22e53f5ba6736dbf7ab3888b09a9
wangkui <184952865@qq.com>
2017-07-15 00:46:34 +0800

    First version that is ready for deployment.

--- tools/installation.sh
+++ tools/installation.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
 cd /var/www/
-rm -r PySite
+
+rm -r -f PySite
 git clone http://github.com/revoll/PySite.git
 chown -R www-data:www-data PySite
 mv PySite/.pysite_env.example PySite/.pysite_env
+
 mkdir -p /etc/nginx
 mkdir -p /etc/uwsgi/vassals
+rm -f /etc/nginx/sites-available/pysite_nginx.conf
+rm -f /etc/nginx/sites-enabled/pysite_nginx.conf
+rm -f /etc/uwsgi/vassals/pysite_uwsgi.ini
 cp PySite/tools/pysite_nginx.conf /etc/nginx/sites-available/pysite_nginx.conf
 cp PySite/tools/pysite_uwsgi.ini /etc/uwsgi/vassals/pysite_uwsgi.ini
 ln -s /etc/nginx/sites-available/pysite_nginx.conf /etc/nginx/sites-enabled/pysite_nginx.conf
